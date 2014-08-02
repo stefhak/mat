c7bebc6f18e9f204bfbf4930ccca7d374aa6f459
Stefan Hakansson LK <stefhak@gmail.com>
2014-08-02 10:31:55 +0200

    Added support for swiping to coming and past weeks. Unfortunately swipe only works on simulator so far.

--- stepposcript.js
+++ stepposcript.js
@@ -5,6 +5,8 @@ var skola = new Object();
 var days = ["Mån", "Tis", "Ons", "Tors", "Fre"];
 var meny;
 var backbutton;
+var startPos;
+var weekIx;
 
 
 function clear () {
@@ -25,38 +27,116 @@ function clear () {
       anchor.removeChild(anchor.firstChild);
   }
 }
-function display_meny () {
+function touchStart (e) {
+  console.log("x-pos: " + e.touches[0].pageX + " y-pos: " + e.touches[0].pageY);
+  startPos = e;
+}
+function touchEnd (e) {
+  console.log("x-pos: " + e.touches[0].pageX + " y-pos: " + e.touches[0].pageY);
+  if (e.touches[0].pageX > (startPos.touches[0].pageX + 30)) {
+    console.log("right swipe detected");
+    if (weekIx > 0) {
+      weekIx--;
+      display_meny(weekIx);
+    }
+  } else if ((e.touches[0].pageX + 30) < startPos.touches[0].pageX) {
+    console.log("leftt swipe detected");
+    if (weekIx < 4) {
+      weekIx++;
+      display_meny(weekIx);
+    }
+
+
+  }
+  
+}
+
+function detectSwipe () {
+  var touchzone = document.getElementById("main");
+  touchzone.addEventListener("touchstart", touchStart, false);
+  touchzone.addEventListener("touchend", touchEnd, false);
+}
+
+function display_meny_page () {
   clear();
   console.log("skola vald");
   backbutton = new Image();
   backbutton.src = "back.png";
   backbutton.addEventListener("click", display_skolor);
   document.getElementById("header_left").appendChild(backbutton);
-  var textnode = document.createTextNode(skola.name);
-  document.getElementById("header_center").appendChild(textnode);
-  document.getElementById("header_center").appendChild(document.createElement("br"));
-  textnode = document.createTextNode(kommun.name);
-  document.getElementById("header_center").appendChild(textnode);
-  document.getElementById("header_center").appendChild(document.createElement("br"));
+  var para = document.createElement("p");
+  para.innerHTML = skola.name;
+  para.style.fontSize = "18px";
+  document.getElementById("header_center").appendChild(para);
+  //document.getElementById("header_center").appendChild(document.createElement("br"));
+  para = document.createElement("p");
+  //ktext.style.color = "red";
+  para.innerHTML = kommun.name;
+  document.getElementById("header_center").appendChild(para);
+  //document.getElementById("header_center").appendChild(document.createElement("br"));
+  para = document.createElement("p");
+  para.id = "weekNo";
+  document.getElementById("header_center").appendChild(para);
+  var logo = new Image();
+  logo.src = kommun.listOfSchools[0].logo_url;
+  document.getElementById("header_right").appendChild(logo);
+  var tabell = document.createElement("table");
+  tabell.id = "tabellen";
+  document.getElementById("main").appendChild(tabell);
+  var row, cell;
+  days.forEach(function (el, ix, arr) {
+    row = document.createElement("tr");
+    row.id = el;
+    row.className = "day_row";
+    tabell.appendChild(row);
+    cell = document.createElement("td");
+    cell.id = el + "date";
+    cell.className = "day_date_cell";
+    row.appendChild(cell);
+    para = document.createElement("p");
+    para.id = el + "dateday";
+    para.className = "day_date_day";
+    cell.appendChild(para);
+    para = document.createElement("p");
+    para.id = el + "datedate";
+    para.className = "day_date_date";
+    cell.appendChild(para);
+    cell = document.createElement("td");
+    cell.id = el + "meal";
+    cell.className = "day_meal_cell";
+    row.appendChild(cell);
+  });
+}
+function display_meny (weekIndex) {
+  if (weekIndex == 2) {
+    document.body.style.background = "#99CCFF"
+  } else {
+    document.body.style.background = "#CCFFFF";
+  }
+  document.getElementById("weekNo").innerHTML = "Vecka " + meny.weeks[weekIndex].week;
+  for (var i = 0; i < days.length; i++) {
+    document.getElementById(days[i] + "dateday").innerHTML = days[i];
+    document.getElementById(days[i] + "datedate").innerHTML = meny.weeks[weekIndex].days[i].date;
+    if (meny.weeks[weekIndex].days[i].items) {
+      for (var j = 0; j < meny.weeks[weekIndex].days[i].items.length; j++) {
+        document.getElementById(days[i] + "meal").innerHTML = meny.weeks[weekIndex].days[i].items[j];
+        document.getElementById(days[i] + "meal").innerHTML += "<br />";
+      }
+    } else if (meny.weeks[weekIndex].days[i].reason) {
+     document.getElementById(days[i] + "meal").innerHTML = meny.weeks[weekIndex].days[i].reason;
+    }
+  }
+}
+function skola_vald () {
+  skola = kommun.listOfSchools[this.skola];
+  weekIx = 2;
+  display_meny_page();
   function reqListener () {
     console.log("got response2");
     console.log(this.responseText);
     meny = JSON.parse(this.responseText);
-    textnode = document.createTextNode("Meny vecka " + meny.weeks[2].week );
-    document.getElementById("header_center").appendChild(textnode);
-    var logo = new Image();
-    logo.src = kommun.listOfSchools[0].logo_url;
-    document.getElementById("header_right").appendChild(logo);
-    var anchor = document.getElementById("main");
-    for (var i = 0; i < 5; i++) {
-      anchor.innerHTML += days[i] + " " + meny.weeks[2].days[i].date + ": ";
-      if (meny.weeks[2].days[i].item) {
-        anchor.innerHTML += meny.weeks[2].days[i].item;
-      } else if (meny.weeks[2].days[i].reason) {
-        anchor.innerHTML += meny.weeks[2].days[i].reason;
-      }
-      anchor.innerHTML += "<br />";
-    }
+    display_meny(weekIx);
+    detectSwipe();
   }
   console.log("got to get matsedel");
   var oReq = new XMLHttpRequest({mozSystem: true});
@@ -64,10 +144,6 @@ function display_meny () {
   oReq.open("get", skola.url + "?fmt=json&v=2&limit=5&offset=-2", true);
   oReq.send();
 
-}
-function skola_vald () {
-  skola = kommun.listOfSchools[this.skola];
-  display_meny();
 }
 function display_skolor () {
   clear();
@@ -154,6 +230,7 @@ function display_lan () {
     anchor.appendChild(node);
   }
 }
+
 function start () {
   function reqListener () {
     console.log("got response");
@@ -166,6 +243,5 @@ function start () {
   oReq.onload = reqListener;
   oReq.open("get", "http://meny.dinskolmat.se/?fmt=json", true);
   oReq.send();
-
 }
 window.addEventListener('load', start);
